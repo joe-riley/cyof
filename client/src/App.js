3160f94735584966aa661057a91496251cf2a91f
Joe Riley <joeyriley@gmail.com>
2021-03-27 17:10:50 -0700

    Save point

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import ApolloClient from "apollo-boost";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './pages/LandingPage';
+import DashboardPage from './pages/DasboardPage'
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -24,6 +25,7 @@ function App() {
         <>
           <Navbar />
           <Switch>
+            <Route exact path="/dashboard" component={DashboardPage} />
             <Route exact path="/" component={LandingPage} />
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
           </Switch>
